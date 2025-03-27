2990ecc357d12c15c079e95adade0bb8f316f1f1
Rogudev <rgutierrez@protonmail.com>
2025-03-27 19:26:50 +0100

    restore prev conf

--- reset.py
+++
@@ -1,8 +0,0 @@
-# load db.reset.json
-with open('db.reset.json', 'r') as file:
-    reset_task = json.load(file)
-    reset_task.sort(key=lambda x: x['finalized'], reverse=False)
-
-    # save the db.reset in db to reset it
-    with open('db.json', 'w') as file:
-        json.dump(reset_task, file, indent=4)
